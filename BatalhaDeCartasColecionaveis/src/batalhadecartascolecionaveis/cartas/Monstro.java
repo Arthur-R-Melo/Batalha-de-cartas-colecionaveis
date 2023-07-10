9ad7f326cd84eff40ea807004566557372409271
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-07-10 13:46:40 -0300

    Update Monstro.java

--- BatalhaDeCartasColecionaveis/src/batalhadecartascolecionaveis/cartas/Monstro.java
+++ BatalhaDeCartasColecionaveis/src/batalhadecartascolecionaveis/cartas/Monstro.java
@@ -10,12 +10,24 @@ package batalhadecartascolecionaveis.cartas;
  */
 public class Monstro extends Carta{
     private Equipamento equipamento;
-    private boolean estado;
+    private boolean estado;//verdadeiro para defesa e falso para ataque
     private boolean atacou;
 
     public Monstro(String nome, String descricao, int atk, int def) {
         super(nome, descricao, atk, def);
+        this.atacou = false;
     }
     
+    public void alteraEstado(int estadoInt) {// 0 para defesa e 1 para ataque
+        this.estado = (estadoInt == 0)
+    }
     
+    public int atacaDefende() {
+        //TODO
+    }
+    public void equipaItem(Equipamento equipamento) {
+        if(this.equipamento == null) {
+            this.equipamento = equipamento;
+        }
+    }
 }

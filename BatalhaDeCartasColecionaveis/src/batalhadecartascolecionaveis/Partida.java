90bc82e69505b3153c0637caf5e758afd81d42df
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-08-12 17:21:55 -0300

    Update Partida.java

--- BatalhaDeCartasColecionaveis/src/batalhadecartascolecionaveis/Partida.java
+++ BatalhaDeCartasColecionaveis/src/batalhadecartascolecionaveis/Partida.java
@@ -1,5 +1,7 @@
 package batalhadecartascolecionaveis;
 
+import java.util.Scanner;
+
 import batalhadecartascolecionaveis.jogadores.Jogador;
 
 public class Partida {
@@ -9,5 +11,28 @@ public class Partida {
     public Partida(Jogador[] jogadores) {
         this.jogadores = jogadores;
         this.tabuleiro = new Tabuleiro();
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int jogarNovamente;
+        do {
+            Jogador[] jogadores = new Jogador[2];
+            System.out.println("Deseja jogar contra a máquina(1) ou contra outro jogador(2)?");
+            int num = s.nextInt();
+            jogadores[0] = new Jogador(0);
+            while (num != 1 && num != 2) {
+                System.out.println("Insira um número válido: ");
+                num = s.nextInt();
+            }
+            if (num == 1) {
+                //TODO
+                jogadores[1] = new Jogador(1);
+            } else {
+                jogadores[1] = new Jogador(1);
+            }
+            System.out.println("Deseja jogar novamente?(1 - sim/outro valor - não)");
+            jogarNovamente = s.nextInt();
+        } while (jogarNovamente == 1);
     }
 }
